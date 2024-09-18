d7f12e569539e70334ecf32c7f2a5f54119d0ee1
Miriam Blanco <m.blancoga@gmail.com>
2024-09-18 18:06:11 +0200

    First steps building the web and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [gameTitle, setGameTitle] = useState("");
+
+  const videogameApiLink =
+    "https://www.cheapshark.com/api/1.0/games?title=batman";
+
+  const searchGame = () => {
+    fetch(videogameApiLink).then((response) =>
+      response.json().then((data) => console.log(data))
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="searchSection">
+        <h1>Search For A Game</h1>
+        <input
+          type="text"
+          placeholder="World of Warcraft..."
+          onChange={(event) => {
+            setGameTitle(event.target.value);
+          }}
+        />
+        <button onClick={searchGame}>Search Game Title</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="dealsSection">
+        <h1>Latest Deals 🔥</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
